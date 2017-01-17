955bbec44dfae9fde957dda4acb73422355cb0b6
HanYoungsoo <gglife333@gmail.com>
2017-01-17 11:41:16 +0900

    Person class commit

---
+++ MySecondIos/MySecondIos/Person.h
@@ -0,0 +1,29 @@
+//
+//  Person.h
+//  MySecondIos
+//
+//  Created by HanYoungsoo on 2017. 1. 17..
+//  Copyright © 2017년 YoungsooHan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+//속성
+@property id gender;
+@property id age;
+@property id job;
+@property id birth;
+@property id mobileNum;
+@property id address;
+
+- (id)eat;
+- (id)sleep;
+- (id)think;
+- (id)poo;
+- (id)love;
+- (id)pray;
+- (id)hate;
+
+@end
