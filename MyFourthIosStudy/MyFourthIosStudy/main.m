a6e58dbb46d1fa4901f5c1717b57666a88033a72
HanYoungsoo <gglife333@gmail.com>
2017-01-18 18:10:37 +0900

    Fourth Ios Study project

---
+++ MyFourthIosStudy/MyFourthIosStudy/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MyFourthIosStudy
+//
+//  Created by HanYoungsoo on 2017. 1. 18..
+//  Copyright © 2017년 YoungsooHan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
