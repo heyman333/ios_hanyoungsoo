8f029b61f7d3b2fda4d0107ed1fc50dc288edb53
HanYoungsoo <gglife333@gmail.com>
2017-03-20 15:17:35 +0900

    네트워크 미니 프로젝트 파일 이

---
+++ Category/Category/UIView+AutoImageSave.m
@@ -0,0 +1,12 @@
+//
+//  UIView+AutoImageSave.m
+//  Category
+//
+//  Created by HanYoungsoo on 2017. 3. 20..
+//  Copyright © 2017년 YoungsooHan. All rights reserved.
+//
+
+#import "UIView+AutoImageSave.h"
+
+@implementation UIView (AutoImageSave)
+@end
